f6dc4f659f68580b8b750390ee13797efc36279f
Shank <nicatron21@gmail.com>
2011-05-29 19:24:32 -0600

    Started crap.

---
+++ Terraria/ShankShockCommandProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Terraria
+{
+	class ShankShockCommandProcessor
+	{
+        private string[] commands =
+        {
+            "hardcore",
+            "break",
+            "skeletron",
+            "reload",
+            "bloodmoon",
+            "dropmeteor",
+            "eye",
+            "invade",
+            "help",
+            "spawn",
+            "kc",
+            "eater",
+            "testadmin",
+            "kick",
+            "ban",
+        };
+
+
+	}
+}
