d1438b11ca665c85110441428d3132adfc7f9f74
Maverick Motherfucker <MMavipc@hostedyou.com>
2011-06-07 18:28:17 -0700

    started work on Updater

---
+++ Update/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace Update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            string procname;
+            try
+            {
+                StreamReader sr = new StreamReader("pn");
+                procname = sr.ReadToEnd();
+                sr.Close();
+                sr.Dispose();
+                sr = new StreamReader("pid");
+                data = sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                data = "";
+            }
+
+            try
+            {
+                Process TServer = Process.GetProcessById(Convert.ToInt32(data));
+                while (!TServer.HasExited)
+                {
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                File.Delete("TShockAPI.dll");
+            }
+            catch (FileNotFoundException)
+            {
+            }
+
+            BinaryWriter bw = new BinaryWriter(new FileStream("TShockAPI.dll", FileMode.Create));
+            bw.Write(Resources.TShockAPI);
+            bw.Close();
+        }
+    }
+}
