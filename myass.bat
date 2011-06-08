451aa9c8ae0a20c8c6c80e1dd31a6872cbb48a00
high <pooop828@hotmail.com>
2011-06-08 11:36:03 -0400

    missing pdb

--- myass.bat
+++ myass.bat
@@ -1,2 +1,4 @@
 attrib -r "C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins\TShockAPI.dll"
+attrib -r "C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins\TShockAPI.pdb"
 copy "TShockAPI.dll" "C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins"
+copy "TShockAPI.pdb" "C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins"
