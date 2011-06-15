91a0b82d3b6ca3056c83779cdd6561133ac2e058
ricky <rjhazelwood@gmail.com>
2011-06-15 20:56:01 +1000

    Dumbass

--- TShockAPI/Commands.cs
+++ TShockAPI/Commands.cs
@@ -821,7 +821,7 @@ namespace TShockAPI
             int killcount = 0;
             for (int i = 0; i < Main.npc.Length; i++)
             {
-                if (!Main.npc[i].townNPC && !Main.npc[i].active)
+                if (!Main.npc[i].townNPC && Main.npc[i].active)
                 {
                     Main.npc[i].StrikeNPC(99999, 90f, 1);
                     NetMessage.SendData(28, -1, -1, "", i, 99999, 90f, 1);
