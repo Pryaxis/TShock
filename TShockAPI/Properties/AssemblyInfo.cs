72bc9a9afe4b3c51182678dbaf555c7750e03e01
Lucas Nicodemus <nicatron21@gmail.com>
2011-06-29 00:22:47 -0600

    Version tick

--- TShockAPI/Properties/AssemblyInfo.cs
+++ TShockAPI/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.3.0.0627")]
-[assembly: AssemblyFileVersion("2.3.0.0627")]
+[assembly: AssemblyVersion("2.3.0.0629")]
+[assembly: AssemblyFileVersion("2.3.0.0629")]
