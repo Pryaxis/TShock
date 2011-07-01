d55b8cb4fbbe45cf6d06cbab51cd60c2c049d449
high <pooop828@hotmail.com>
2011-07-01 11:07:23 -0400

    Readded postbuild.bat and fixed so it only copies if the target directory exists.

--- myass.bat
+++
@@ -1,4 +0,0 @@
-attrib -r "C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins\TShockAPI.dll"
-attrib -r "C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins\TShockAPI.pdb"
-copy "TShockAPI.dll" "C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins"
-copy "TShockAPI.pdb" "C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins"
---
+++ postbuild.bat
@@ -0,0 +1,12 @@
+@echo off
+set pluginspath=C:\Program Files (x86)\Steam\steamapps\common\terraria\serverplugins\
+IF NOT EXIST "%pluginspath%" GOTO SkipCopy
+attrib -r "%pluginspath%TShockAPI.dll"
+attrib -r "%pluginspath%TShockAPI.pdb"
+copy "TShockAPI.dll" "%pluginspath%"
+copy "TShockAPI.pdb" "%pluginspath%"
+echo Files copied
+GOTO end
+:SkipCopy
+echo Skipped copying files
+:end
