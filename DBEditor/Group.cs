ca9c5a5d4a4311c747b0d3d6e0bd1d8e9a8e581a
Steven French <mr.steven.french@gmail.com>
2011-07-23 14:41:03 +1200

    Adds DBEditor to Master Project
    DBEditor can now add new groups

---
+++ DBEditor/Group.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TShockDBEditor
+{
+    public class Group
+    {
+        public readonly List<string> permissions = new List<string>();
+        private readonly List<string> negatedpermissions = new List<string>();
+
+        public int ID { get; protected set; }
+        public string Name { get; set; }
+        public Group Parent { get; set; }
+        public int Order { get; set; }
+
+        public Group(int id, string groupname, int order, Group parentgroup = null)
+        {
+            Order = order;
+            ID = id;
+            Name = groupname;
+            Parent = parentgroup;
+        }
+    }
+}
