f1ed9207c115e31bfe5e9c8642ce5ef5eb6d937e
Lucas Nicodemus <nicatron21@gmail.com>
2011-07-23 15:14:27 -0600

    New tests

--- TShockAPI/Properties/AssemblyInfo.cs
+++ TShockAPI/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.2.0722")]
-[assembly: AssemblyFileVersion("3.0.2.0722")]
+[assembly: AssemblyVersion("3.0.2.0723")]
+[assembly: AssemblyFileVersion("3.0.2.0723")]
---
+++ UnitTests/BanManagerTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Community.CsharpSqlite.SQLiteClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BanManagerTest
+    {
+        public static IDbConnection DB;
+        private BanManager Bans;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            TShock.Config = new ConfigFile();
+            TShock.Config.StorageType = "sqlite";
+
+            DB = new SqliteConnection(string.Format("uri=file://{0},Version=3", "tshock.test.sqlite"));
+            DB.Open();
+
+            Bans = new BanManager(DB);
+        }
+
+        [TestMethod]
+        public void TestDBNotNull()
+        {
+            Assert.IsNotNull(Bans);
+        }
+
+        [TestMethod]
+        public void AddBanTest()
+        {
+            Assert.IsTrue(Bans.AddBan("127.0.0.1", "BanTest", "Ban Testing"));
+        }
+
+        [TestMethod]
+        public void FindBanTest()
+        {
+            Assert.IsNotNull(Bans.GetBanByIp("127.0.0.1"));
+            TShock.Config.EnableBanOnUsernames = true;
+            Assert.IsNotNull(Bans.GetBanByName("BanTest"));
+            TShock.Config.EnableBanOnUsernames = false;
+            Assert.IsNull(Bans.GetBanByName("BanTest"));
+        }
+    }
+}
