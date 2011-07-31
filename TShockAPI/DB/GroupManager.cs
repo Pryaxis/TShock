75f3d3c0466b0c4207607613e2cb33438d0b99cd
Deathmax <death@deathmax.tk>
2011-07-31 22:56:44 +0800

    out of range check when reading chat colors.

--- TShockAPI/DB/GroupManager.cs
+++ TShockAPI/DB/GroupManager.cs
@@ -187,9 +187,12 @@ namespace TShockAPI.DB
                             group.AddPermission(commands[i].Trim());
                         }
                         String[] chatcolour = reader.Get<String>("ChatColor").Split(',');
-                        byte.TryParse(chatcolour[0], out group.R);
-                        byte.TryParse(chatcolour[1], out group.G);
-                        byte.TryParse(chatcolour[2], out group.B);
+                        if (chatcolour.Length == 3)
+                        {
+                            byte.TryParse(chatcolour[0], out group.R);
+                            byte.TryParse(chatcolour[1], out group.G);
+                            byte.TryParse(chatcolour[2], out group.B);
+                        }
                         groups.Add(group);
                     }
                 }
