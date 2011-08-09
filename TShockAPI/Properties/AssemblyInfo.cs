552dff56eed9d6e34209cfa89194971dcc1768cb
high <pooop828@hotmail.com>
2011-08-09 18:28:08 -0400

    update version tick

--- TShockAPI/Properties/AssemblyInfo.cs
+++ TShockAPI/Properties/AssemblyInfo.cs
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("3.2.1.0809")]
-[assembly: AssemblyFileVersion("3.2.1.0809")]
+[assembly: AssemblyVersion("3.2.2.0809")]
+[assembly: AssemblyFileVersion("3.2.2.0809")]
