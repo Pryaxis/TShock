b52e6a83c8631ad7cab4e2239d0f64bf489255f4
Lucas Nicodemus <nicatron21@gmail.com>
2012-01-01 03:10:43 -0700

    Re-add MaxMind's license

--- TShockAPI/GeoIPCountry.cs
+++ TShockAPI/GeoIPCountry.cs
@@ -1,4 +1,23 @@
-﻿/*
+﻿/* GeoIPCountry.cs
+ *
+ * Copyright (C) 2008 MaxMind, Inc.  All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
 TShock, a server mod for Terraria
 Copyright (C) 2011 The TShock Team
 
