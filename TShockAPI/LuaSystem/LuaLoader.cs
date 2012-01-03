c45291eb6ba51e1066142e81cc3fce2e24782677
Lucas Nicodemus <nicatron21@gmail.com>
2012-01-03 16:54:38 -0700

    Basic Lua loading prep. TSHOCKPLUGINS-4

---
+++ TShockAPI/LuaSystem/LuaLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LuaInterface;
+
+namespace TShockAPI.LuaSystem
+{
+	public class LuaLoader
+	{
+		private Lua Lua = null;
+		public string LuaPath = "";
+		public LuaLoader(string path)
+		{
+			Lua = new Lua();
+			LuaPath = path;
+			SendLuaDebugMsg("Lua 5.1 (serverside) initialized.");
+		}
+
+		public void LoadServerAutoruns()
+		{
+			foreach (string s in Directory.GetFiles(Path.Combine(LuaPath, "autorun")))
+			{
+				Lua.DoFile(s);
+				SendLuaDebugMsg("Loaded file: " + s);
+			}
+		}
+
+		public void SendLuaDebugMsg(string s)
+		{
+			ConsoleColor previousColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("Lua: " + s);
+			Console.ForegroundColor = previousColor;
+		}
+	}
+}
