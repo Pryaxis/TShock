15d9a2aa8c9fc3e1ad38783d9b69f082f5cb23ff
Lucas Nicodemus <nicatron21@gmail.com>
2012-01-03 17:44:54 -0700

    More basic prep. TSHOCKPLUGINS-4

--- TShockAPI/LuaSystem/LuaLoader.cs
+++ TShockAPI/LuaSystem/LuaLoader.cs
@@ -11,19 +11,45 @@ namespace TShockAPI.LuaSystem
 	{
 		private Lua Lua = null;
 		public string LuaPath = "";
+		public string LuaAutorunPath = "";
 		public LuaLoader(string path)
 		{
 			Lua = new Lua();
 			LuaPath = path;
+			LuaAutorunPath = Path.Combine(LuaPath, "autorun");
 			SendLuaDebugMsg("Lua 5.1 (serverside) initialized.");
+
+			if (!Directory.Exists(LuaPath))
+			{
+				Directory.CreateDirectory(LuaPath);
+				Directory.CreateDirectory(LuaAutorunPath);
+			}
+
+			RegisterLuaFunctions();
+			LoadServerAutoruns();
 		}
 
 		public void LoadServerAutoruns()
 		{
-			foreach (string s in Directory.GetFiles(Path.Combine(LuaPath, "autorun")))
+			try
 			{
-				Lua.DoFile(s);
-				SendLuaDebugMsg("Loaded file: " + s);
+				foreach (string s in Directory.GetFiles(LuaAutorunPath))
+				{
+					SendLuaDebugMsg("Loading: " + s);
+					try
+					{
+						Lua.DoFile(s);
+					}
+					catch (LuaException e)
+					{
+						SendLuaDebugMsg(e.Message);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				SendLuaDebugMsg(e.Message);
+				SendLuaDebugMsg(e.StackTrace);
 			}
 		}
 
@@ -32,6 +58,23 @@ namespace TShockAPI.LuaSystem
 			ConsoleColor previousColor = Console.ForegroundColor;
 			Console.ForegroundColor = ConsoleColor.Cyan;
 			Console.WriteLine("Lua: " + s);
+			Console.ForegroundColor = previousColor;
+		}
+
+		public void RegisterLuaFunctions()
+		{
+			LuaFunctions LuaFuncs = new LuaFunctions();
+			Lua.RegisterFunction("Print", LuaFuncs, LuaFuncs.GetType().GetMethod("Print"));
+		}
+	}
+
+	public class LuaFunctions
+	{
+		public void Print(string s)
+		{
+			ConsoleColor previousColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine(s);
 			Console.ForegroundColor = previousColor;
 		}
 	}
