e0643c596fadb4f468265723b85db953c24dd74d
Steven French <mr.steven.french@gmail.com>
2012-01-08 22:17:04 +1300

    Working Lua Plugin

---
+++ LuaPlugin/LuaLoader.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.ComponentModel;
+using LuaInterface;
+using TShock;
+using TShock.Hooks.Player;
+
+namespace LuaPlugin
+{
+	public class LuaLoader : LuaPlugin
+	{
+		private readonly Lua _lua = null;
+		public string LuaPath = "";
+		public string LuaAutorunPath = "";
+		public LuaLoader(string path)
+		{
+			_lua = new Lua();
+			LuaPath = path;
+			LuaAutorunPath = Path.Combine(LuaPath, "autorun");
+			SendLuaDebugMsg("Lua 5.1 (serverside) initialized.");
+
+			if (!string.IsNullOrEmpty(LuaPath) && !Directory.Exists(LuaPath))
+			{
+				Directory.CreateDirectory(LuaPath);
+			}
+			if (!string.IsNullOrEmpty(LuaAutorunPath) && !Directory.Exists(LuaAutorunPath))
+			{
+				Directory.CreateDirectory(LuaAutorunPath);
+			}
+
+			RegisterLuaFunctions();
+			LoadServerAutoruns();
+		}
+
+		public void LoadServerAutoruns()
+		{
+			try
+			{
+				foreach (string s in Directory.GetFiles(LuaAutorunPath))
+				{
+					SendLuaDebugMsg("Loading: " + s);
+					RunLuaFile(s);
+				}
+			}
+			catch (Exception e)
+			{
+				SendLuaDebugMsg(e.Message);
+				SendLuaDebugMsg(e.StackTrace);
+			}
+		}
+
+		public void RunLuaString(string s)
+		{
+			try
+			{
+				_lua.DoString(s);
+			}
+			catch (Exception e)
+			{
+				SendLuaDebugMsg(e.Message);
+			}
+		}
+
+		public void RunLuaFile(string s)
+		{
+			try
+			{
+				_lua.DoFile(s);
+			}
+			catch (LuaException e)
+			{
+				SendLuaDebugMsg(e.Message);
+			}
+		}
+
+		public void SendLuaDebugMsg(string s)
+		{
+			ConsoleColor previousColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine("Lua: " + s);
+			Console.ForegroundColor = previousColor;
+		}
+
+		public void RegisterLuaFunctions()
+		{
+			_lua["PlayerHooks"] = Hooks.PlayerHooks;
+			_lua["Game"] = Game;
+			_lua["Color"] = new Color();
+			_lua["Players"] = Game.Players;
+			//More Lua Functions
+			LuaFunctions LuaFuncs = new LuaFunctions(this);
+			var LuaFuncMethods = LuaFuncs.GetType().GetMethods();
+			foreach (System.Reflection.MethodInfo method in LuaFuncMethods)
+			{
+				_lua.RegisterFunction(method.Name, LuaFuncs, method);
+			}
+		}
+	}
+
+	public class LuaFunctions
+	{
+		LuaLoader Parent;
+		public LuaFunctions(LuaLoader parent)
+		{
+			Parent = parent;
+		}
+
+		[Description("Prints a message to the console from the Lua debugger.")]
+		public void Print(string s)
+		{
+			ConsoleColor previousColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.WriteLine(s);
+			Console.ForegroundColor = previousColor;
+		}
+	}
+}
---
+++ LuaPlugin/LuaPlugin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShock;
+using TShock.Hooks.Player;
+
+namespace LuaPlugin
+{
+	public class LuaPlugin : Plugin
+	{
+		public override string Name
+		{
+			get { return "Lua Loader"; }
+		}
+
+		public override Version Version
+		{
+			get { return new Version(1, 0); }
+		}
+
+		public override Version ApiVersion
+		{
+			get { return new Version(1, 10); }
+		}
+
+		public override string Author
+		{
+			get { return "The Nyx Team"; }
+		}
+
+		public override string Description
+		{
+			get { return "Adds Lua support for Terraria API"; }
+		}
+
+		public override bool Enabled
+		{
+			get;
+			set;
+		}
+
+		public override void Initialize()
+		{
+		}
+	}
+}
