c0b2c4eed22a17239023ca22c7d1916aa6713108
Lucas Nicodemus <nicatron21@gmail.com>
2012-01-21 23:18:31 -0700

    ETA

--- TShockAPI/TShock.cs
+++ TShockAPI/TShock.cs
@@ -41,7 +41,7 @@ namespace TShockAPI
 	public class TShock : TerrariaPlugin
 	{
 		public static readonly Version VersionNum = Assembly.GetExecutingAssembly().GetName().Version;
-		public static readonly string VersionCodename = "1.1.2 was sudden";
+		public static readonly string VersionCodename = "ETA 1am 1/22/2012 MDT";
 
 		public static string SavePath = "tshock";
 
