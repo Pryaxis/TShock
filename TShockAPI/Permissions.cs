c62bd442ec9c5f3f621c8d5adbdf94eec1eca844
Lucas Nicodemus <nicatron21@gmail.com>
2012-02-25 23:28:18 -0700

    Permissions now outputs a better markdown dump

--- TShockAPI/Permissions.cs
+++ TShockAPI/Permissions.cs
@@ -200,7 +200,7 @@ namespace TShockAPI
 						c =>
 						c.Name + (c.Names.Count > 1 ? "({0})".SFormat(string.Join(" ", c.Names.ToArray(), 1, c.Names.Count - 1)) : ""));
 
-				sb.AppendLine("## <a name=\"{0}\">{0}</a>  ".SFormat(name));
+				sb.AppendLine("## <a id=\"{0}\">{0}</a>  ".SFormat(name));
 				sb.AppendLine("**Description:** {0}  ".SFormat(desc));
 				sb.AppendLine("**Commands:** {0}  ".SFormat(strs.Count() > 0 ? string.Join(" ", strs) : "None"));
 				sb.AppendLine();
