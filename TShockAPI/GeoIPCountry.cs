8d5cdf04a8a7df92fecd4748a8a8bae5aeb41d96
Lucas Nicodemus <shank@shanked.me>
2012-04-24 12:45:46 -0600

    Update copyright to reflect actual year date.

--- TShockAPI/GeoIPCountry.cs
+++ TShockAPI/GeoIPCountry.cs
@@ -19,7 +19,7 @@
 
 /*
 TShock, a server mod for Terraria
-Copyright (C) 2011 The TShock Team
+Copyright (C) 2011-2012 The TShock Team
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
