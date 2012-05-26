5394734a33e07786c9c8c4e06cc230baf753c745
Lucas Nicodemus <shank@shanked.me>
2012-05-25 20:10:19 -0600

    Removed the last two changes without revertting

--- TShockAPI/ConfigFile.cs
+++ TShockAPI/ConfigFile.cs
@@ -216,9 +216,9 @@ namespace TShockAPI
 
 	    [Description("Allow Ice placement even when user does not have canbuild")] public bool AllowIce = false;
 
-	    [Description("Allows corrutption to spread when a world is hardmode.")] public bool AllowHardmodeCorruptionCreep = true;
+	    [Description("Allows corrutption to spread when a world is hardmode.")] public bool AllowCorruptionCreep = true;
 
-        [Description("Allows hallow to spread when a world is hardmode.")] public bool AllowHardmodeHallowCreep = true;
+        [Description("Allows hallow to spread when a world is hardmode.")] public bool AllowHallowCreep = true;
 
         [Description("How many things a statue can spawn within 200 pixels(?) before it stops spawning. Default = 3")] public int StatueSpawn200 = 3;
 
