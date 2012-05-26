9836406cfc40b00023619efa1e369811dac2cb3a
Lucas Nicodemus <shank@shanked.me>
2012-05-26 11:19:36 -0600

    In the mess of a method that is OnGetData, it makes no sense to logically check to see if something is both greater than 12 but not 5.

--- TShockAPI/TShock.cs
+++ TShockAPI/TShock.cs
@@ -893,7 +893,7 @@ namespace TShockAPI
 			}
 
 			if ((player.State < 10 || player.Dead) && (int) type > 12 && (int) type != 16 && (int) type != 42 && (int) type != 50 &&
-				(int) type != 38 && (int) type != 5 && (int) type != 21)
+				(int) type != 38 && (int) type != 21)
 			{
 				e.Handled = true;
 				return;
