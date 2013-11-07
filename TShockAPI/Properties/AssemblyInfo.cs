691204f319b1acc88948b4bd3f8e39b6ea80857c
Zack Piispanen <olink@shankshock.com>
2013-11-07 05:00:26 -0500

    Version Tick

--- TShockAPI/Properties/AssemblyInfo.cs
+++ TShockAPI/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      MMdd of the build
 
-[assembly: AssemblyVersion("4.2.0.1019")]
-[assembly: AssemblyFileVersion("4.2.0.1019")]
+[assembly: AssemblyVersion("4.2.1.1107")]
+[assembly: AssemblyFileVersion("4.2.1.1107")]
