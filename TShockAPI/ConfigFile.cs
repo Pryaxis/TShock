2a8d3dd09a5575cc6e1effa902003777181f2a2c
Sam Eaton <nuluvius@gmail.com>
2014-11-08 22:18:27 +0000

    Added npcStreamSpeed configuration parameter.

--- TShockAPI/ConfigFile.cs
+++ TShockAPI/ConfigFile.cs
@@ -388,6 +388,9 @@ namespace TShockAPI
 		[Description("The maximum allowable MP, before equipment buffs.")]
 		public int MaxMP = 200;
 
+        [Description("Reduces enemy skipping but increases bandwidth usage. The lower the number the less skipping will happen, but more data is sent. 0 is off.")]
+	    public int NpcStreamSpeed = 60;
+
 		/// <summary>
 		/// Reads a configuration file from a given path
 		/// </summary>
