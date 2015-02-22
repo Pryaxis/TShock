5788beed1ce93c73d6ffcd1ff2e53d4f8925bf0c
Zack <olink@shankshock.com>
2015-02-21 23:13:52 -0500

    Revert back to using requests...

--- deploy_release.py
+++ deploy_release.py
@@ -1,9 +1,11 @@
-import urllib2
+import requests
 import json 
 import sys 
 import os
 
-req_url = 'https://api.github.com/repos/NyxStudios/TShock/releases'
+create_release_url = 'https://api.github.com/repos/NyxStudios/TShock/releases'
+release_name = 'tshock_release.zip'
+
 branch = os.environ["GIT_BRANCH"]
 tag_name = os.environ["bamboo_tag_name"]
 name = os.environ["bamboo_release_name"]
@@ -11,13 +13,20 @@ name = os.environ["bamboo_release_name"]
 #because we can't find any other secure way to get a token into this script run from bamboo :'(
 with open('/home/bamboo/scripts/token.py') as f:
     token = f.read().rsplit('=', 1)[1].strip()
-
 body = 'This is the newest release for TShock.  Please see the release thread for more information @ http://tshock.co/xf'
 
 data = {'tag_name':tag_name, 'target_commitish':branch, 'name':name, 'body':body, 'draft':False, 'prerelease':False}
-
-headers = {'Content-Type': 'application/json', 'Authorization': 'token ' + token}
-
+create_headers = {'Content-Type': 'application/json', 'Authorization': 'token ' + token}
 json_data = json.dumps(data)
-req = urllib2.Request(req_url, json_data, headers)
-urllib2.urlopen(req)
+
+r = requests.post(create_release_url, data=json_data, headers=create_headers)
+json_response = json.loads(r.text)
+
+release_id = json_response['id']
+upload_url = json_response['upload_url'].rsplit('{')[0]
+upload_url = upload_url + '?name=' + release_name
+
+upload_headers = {'Authorization': 'token ' + token}
+files = {'file': (release_name, open(release_name, 'rb'), 'application/zip')}
+r = requests.post(upload_url, files=files, headers = upload_headers)
+
