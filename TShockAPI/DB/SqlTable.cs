7187245879bc9c33eb4b31b724f0f0fe895f945d
Lucas Nicodemus <shank@shanked.me>
2015-03-10 11:14:21 -0600

    Fix deprecation message fixes #880

--- TShockAPI/DB/SqlTable.cs
+++ TShockAPI/DB/SqlTable.cs
@@ -76,7 +76,7 @@ namespace TShockAPI.DB
         /// Ensures a table exists and that its structure is correct
 		/// </summary>
 		/// <param name="table">The table name</param>
-        [Obsolete("This method will be replaced by EnsureTableExists.")]
+        [Obsolete("This method will be replaced by EnsureTableStructure.")]
 		public void EnsureExists(SqlTable table)
 		{
 			EnsureTableStructure(table);
