49b4effe74a8fbbc8cc746e5cd3a109547921b3a
Adilson Carvalho <lc.adilson@gmail.com>
2016-10-16 17:06:02 -0200

    Changed year to 2016 on all GPL headers

--- TShockAPI/Net/SpawnMsg.cs
+++ TShockAPI/Net/SpawnMsg.cs
@@ -1,6 +1,6 @@
 ﻿/*
 TShock, a server mod for Terraria
-Copyright (C) 2011-2015 Nyx Studios (fka. The TShock Team)
+Copyright (C) 2011-2016 Nyx Studios (fka. The TShock Team)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
