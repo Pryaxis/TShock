e6fe896133a2bf941c71c014a50a442a9251ec86
Enerdy <rodrigo.rente@live.com>
2016-12-11 23:44:13 +0000

    Update version codename Project Mintaka - The First Star

--- TShockAPI/TShock.cs
+++ TShockAPI/TShock.cs
@@ -54,7 +54,7 @@ namespace TShockAPI
 		/// <summary>VersionNum - The version number the TerrariaAPI will return back to the API. We just use the Assembly info.</summary>
 		public static readonly Version VersionNum = Assembly.GetExecutingAssembly().GetName().Version;
 		/// <summary>VersionCodename - The version codename is displayed when the server starts. Inspired by software codenames conventions.</summary>
-		public static readonly string VersionCodename = "Yoraiz0r R0cks";
+		public static readonly string VersionCodename = "Mintaka";
 
 		/// <summary>SavePath - This is the path TShock saves its data in. This path is relative to the TerrariaServer.exe (not in ServerPlugins).</summary>
 		public static string SavePath = "tshock";
