c3cffd1559f7cb02de9598c5bde0d704b597cf9a
White <WhiteXZ@users.noreply.github.com>
2017-02-21 21:17:31 +1030

    BanManager.cs now consistently uses tabs

--- TShockAPI/DB/BanManager.cs
+++ TShockAPI/DB/BanManager.cs
@@ -24,34 +24,34 @@ using MySql.Data.MySqlClient;
 
 namespace TShockAPI.DB
 {
-    /// <summary>
-    /// Class that manages bans.
-    /// </summary>
+	/// <summary>
+	/// Class that manages bans.
+	/// </summary>
 	public class BanManager
 	{
 		private IDbConnection database;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TShockAPI.DB.BanManager"/> class.
-        /// </summary>
-        /// <param name="db">A valid connection to the TShock database</param>
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TShockAPI.DB.BanManager"/> class.
+		/// </summary>
+		/// <param name="db">A valid connection to the TShock database</param>
 		public BanManager(IDbConnection db)
 		{
 			database = db;
 
 			var table = new SqlTable("Bans",
-			                         new SqlColumn("IP", MySqlDbType.String, 16) {Primary = true},
-			                         new SqlColumn("Name", MySqlDbType.Text),
-									 new SqlColumn("UUID", MySqlDbType.Text),
-			                         new SqlColumn("Reason", MySqlDbType.Text),
-                                     new SqlColumn("BanningUser", MySqlDbType.Text),
-                                     new SqlColumn("Date", MySqlDbType.Text),
-                                     new SqlColumn("Expiration", MySqlDbType.Text)
+									new SqlColumn("IP", MySqlDbType.String, 16) { Primary = true },
+									new SqlColumn("Name", MySqlDbType.Text),
+									new SqlColumn("UUID", MySqlDbType.Text),
+									new SqlColumn("Reason", MySqlDbType.Text),
+									new SqlColumn("BanningUser", MySqlDbType.Text),
+									new SqlColumn("Date", MySqlDbType.Text),
+									new SqlColumn("Expiration", MySqlDbType.Text)
 				);
 			var creator = new SqlTableCreator(db,
-			                                  db.GetSqlType() == SqlType.Sqlite
-			                                  	? (IQueryBuilder) new SqliteQueryCreator()
-			                                  	: new MysqlQueryCreator());
+				db.GetSqlType() == SqlType.Sqlite
+					? (IQueryBuilder)new SqliteQueryCreator()
+					: new MysqlQueryCreator());
 			try
 			{
 				creator.EnsureTableStructure(table);
@@ -185,33 +185,33 @@ namespace TShockAPI.DB
 			return null;
 		}
 
-        /// <summary>
-        /// Adds a ban.
-        /// </summary>
-        /// <returns><c>true</c>, if ban was added, <c>false</c> otherwise.</returns>
-        /// <param name="ip">Ip.</param>
-        /// <param name="name">Name.</param>
-        /// <param name="uuid">UUID.</param>
-        /// <param name="reason">Reason.</param>
-        /// <param name="exceptions">If set to <c>true</c> enable throwing exceptions.</param>
-        /// <param name="banner">Banner.</param>
-        /// <param name="expiration">Expiration date.</param>
+		/// <summary>
+		/// Adds a ban.
+		/// </summary>
+		/// <returns><c>true</c>, if ban was added, <c>false</c> otherwise.</returns>
+		/// <param name="ip">Ip.</param>
+		/// <param name="name">Name.</param>
+		/// <param name="uuid">UUID.</param>
+		/// <param name="reason">Reason.</param>
+		/// <param name="exceptions">If set to <c>true</c> enable throwing exceptions.</param>
+		/// <param name="banner">Banner.</param>
+		/// <param name="expiration">Expiration date.</param>
 		public bool AddBan(string ip, string name = "", string uuid = "", string reason = "", bool exceptions = false, string banner = "", string expiration = "")
 		{
 			try
 			{
-                /*
-                 * If the ban already exists, update its entry with the new date
-                 * and expiration details.
-                 */
-                if (GetBanByIp(ip) != null)
-                {
-                    return database.Query("UPDATE Bans SET Date = @0, Expiration = @1 WHERE IP = @2", DateTime.UtcNow.ToString("s"), expiration, ip) == 1;
-                }
-                else
-                {
-                    return database.Query("INSERT INTO Bans (IP, Name, UUID, Reason, BanningUser, Date, Expiration) VALUES (@0, @1, @2, @3, @4, @5, @6);", ip, name, uuid, reason, banner, DateTime.UtcNow.ToString("s"), expiration) != 0;
-                }
+				/*
+				* If the ban already exists, update its entry with the new date
+				* and expiration details.
+				*/
+				if (GetBanByIp(ip) != null)
+				{
+					return database.Query("UPDATE Bans SET Date = @0, Expiration = @1 WHERE IP = @2", DateTime.UtcNow.ToString("s"), expiration, ip) == 1;
+				}
+				else
+				{
+					return database.Query("INSERT INTO Bans (IP, Name, UUID, Reason, BanningUser, Date, Expiration) VALUES (@0, @1, @2, @3, @4, @5, @6);", ip, name, uuid, reason, banner, DateTime.UtcNow.ToString("s"), expiration) != 0;
+				}
 			}
 			catch (Exception ex)
 			{
@@ -222,14 +222,14 @@ namespace TShockAPI.DB
 			return false;
 		}
 
-        /// <summary>
-        /// Removes a ban.
-        /// </summary>
-        /// <returns><c>true</c>, if ban was removed, <c>false</c> otherwise.</returns>
-        /// <param name="match">Match.</param>
-        /// <param name="byName">If set to <c>true</c> by name.</param>
-        /// <param name="casesensitive">If set to <c>true</c> casesensitive.</param>
-        /// <param name="exceptions">If set to <c>true</c> exceptions.</param>
+		/// <summary>
+		/// Removes a ban.
+		/// </summary>
+		/// <returns><c>true</c>, if ban was removed, <c>false</c> otherwise.</returns>
+		/// <param name="match">Match.</param>
+		/// <param name="byName">If set to <c>true</c> by name.</param>
+		/// <param name="casesensitive">If set to <c>true</c> casesensitive.</param>
+		/// <param name="exceptions">If set to <c>true</c> exceptions.</param>
 		public bool RemoveBan(string match, bool byName = false, bool casesensitive = true, bool exceptions = false)
 		{
 			try
@@ -249,10 +249,10 @@ namespace TShockAPI.DB
 			return false;
 		}
 
-        /// <summary>
-        /// Clears bans.
-        /// </summary>
-        /// <returns><c>true</c>, if bans were cleared, <c>false</c> otherwise.</returns>
+		/// <summary>
+		/// Clears bans.
+		/// </summary>
+		/// <returns><c>true</c>, if bans were cleared, <c>false</c> otherwise.</returns>
 		public bool ClearBans()
 		{
 			try
@@ -290,96 +290,96 @@ namespace TShockAPI.DB
 		AddedOldestToNewest
 	}
 
-    /// <summary>
-    /// Model class that represents a ban entry in the TShock database.
-    /// </summary>
+	/// <summary>
+	/// Model class that represents a ban entry in the TShock database.
+	/// </summary>
 	public class Ban
 	{
-        /// <summary>
-        /// Gets or sets the IP address of the ban entry.
-        /// </summary>
-        /// <value>The IP Address</value>
+		/// <summary>
+		/// Gets or sets the IP address of the ban entry.
+		/// </summary>
+		/// <value>The IP Address</value>
 		public string IP { get; set; }
 
-        /// <summary>
-        /// Gets or sets the name.
-        /// </summary>
-        /// <value>The name.</value>
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>The name.</value>
 		public string Name { get; set; }
 
-        /// <summary>
-        /// Gets or sets the Client UUID of the ban
-        /// </summary>
-        /// <value>The UUID</value>
+		/// <summary>
+		/// Gets or sets the Client UUID of the ban
+		/// </summary>
+		/// <value>The UUID</value>
 		public string UUID { get; set; }
 
-        /// <summary>
-        /// Gets or sets the ban reason.
-        /// </summary>
-        /// <value>The ban reason.</value>
+		/// <summary>
+		/// Gets or sets the ban reason.
+		/// </summary>
+		/// <value>The ban reason.</value>
 		public string Reason { get; set; }
 
-        /// <summary>
-        /// Gets or sets the name of the user who added this ban entry.
-        /// </summary>
-        /// <value>The banning user.</value>
-        public string BanningUser { get; set; }
+		/// <summary>
+		/// Gets or sets the name of the user who added this ban entry.
+		/// </summary>
+		/// <value>The banning user.</value>
+		public string BanningUser { get; set; }
 
-        /// <summary>
-        /// Gets or sets the UTC date of when the ban is to take effect
-        /// </summary>
-        /// <value>The date, which must be in UTC</value>
-        public string Date { get; set; }
+		/// <summary>
+		/// Gets or sets the UTC date of when the ban is to take effect
+		/// </summary>
+		/// <value>The date, which must be in UTC</value>
+		public string Date { get; set; }
 
 		/// <summary>
 		/// Gets the <see cref="System.DateTime"/> object representation of the <see cref="Date"/> string.
 		/// </summary>
 		public DateTime DateTime { get; }
 
-        /// <summary>
-        /// Gets or sets the expiration date, in which the ban shall be lifted
-        /// </summary>
-        /// <value>The expiration.</value>
-        public string Expiration { get; set; }
+		/// <summary>
+		/// Gets or sets the expiration date, in which the ban shall be lifted
+		/// </summary>
+		/// <value>The expiration.</value>
+		public string Expiration { get; set; }
 
 		/// <summary>
 		/// Gets the <see cref="System.DateTime"/> object representation of the <see cref="Expiration"/> string.
 		/// </summary>
 		public DateTime ExpirationDateTime { get; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TShockAPI.DB.Ban"/> class.
-        /// </summary>
-        /// <param name="ip">Ip.</param>
-        /// <param name="name">Name.</param>
-        /// <param name="uuid">UUID.</param>
-        /// <param name="reason">Reason.</param>
-        /// <param name="banner">Banner.</param>
-        /// <param name="date">UTC ban date.</param>
-        /// <param name="exp">Expiration time</param>
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TShockAPI.DB.Ban"/> class.
+		/// </summary>
+		/// <param name="ip">Ip.</param>
+		/// <param name="name">Name.</param>
+		/// <param name="uuid">UUID.</param>
+		/// <param name="reason">Reason.</param>
+		/// <param name="banner">Banner.</param>
+		/// <param name="date">UTC ban date.</param>
+		/// <param name="exp">Expiration time</param>
 		public Ban(string ip, string name, string uuid, string reason, string banner, string date, string exp)
 		{
 			IP = ip;
 			Name = name;
 			UUID = uuid;
 			Reason = reason;
-		    BanningUser = banner;
-		    Date = date;
-		    Expiration = exp;
+			BanningUser = banner;
+			Date = date;
+			Expiration = exp;
 		}
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="TShockAPI.DB.Ban"/> class.
-        /// </summary>
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TShockAPI.DB.Ban"/> class.
+		/// </summary>
 		public Ban()
 		{
 			IP = string.Empty;
 			Name = string.Empty;
 			UUID = string.Empty;
 			Reason = string.Empty;
-		    BanningUser = "";
-		    Date = "";
-		    Expiration = "";
+			BanningUser = "";
+			Date = "";
+			Expiration = "";
 		}
 	}
 }
