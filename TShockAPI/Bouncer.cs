a5cbeb166b0fe034c63372af7d99454d5485008f
Lucas Nicodemus <shank@shanked.me>
2017-12-09 02:15:31 -0700

    Fix some SendTileSquare calls with proper args

--- TShockAPI/Bouncer.cs
+++ TShockAPI/Bouncer.cs
@@ -201,14 +201,14 @@ namespace TShockAPI
 
 			if (!TShock.Utils.TilePlacementValid(x, y))
 			{
-				args.Player.SendTileSquare(tileX, tileY, 4);
+				args.Player.SendTileSquare(x, y, 4);
 				args.Handled = true;
 				return;
 			}
 
 			if (args.Player.Dead && TShock.Config.PreventDeadModification)
 			{
-				args.Player.SendTileSquare(tileX, tileY, 4);
+				args.Player.SendTileSquare(x, y, 4);
 				args.Handled = true;
 				return;
 			}
