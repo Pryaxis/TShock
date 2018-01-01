8d934ef08fc186f63076d5af327504205bbca449
Lucas Nicodemus <shank@shanked.me>
2018-01-01 02:17:21 -0700

    Update to 2018

--- TShockAPI/NetItem.cs
+++ TShockAPI/NetItem.cs
@@ -1,6 +1,6 @@
 /*
 TShock, a server mod for Terraria
-Copyright (C) 2011-2017 Nyx Studios (fka. The TShock Team)
+Copyright (C) 2011-2018 Nyx Studios (fka. The TShock Team)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
