da8b2835716b275c2a4f1d0f5f5431d184efaedc
Kevin Zhao <kzhao1729@gmail.com>
2019-09-29 23:45:40 -0700

    Add FlagAttribute.

---
+++ src/TShock/Commands/Parsers/FlagAttribute.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) 2019 Pryaxis & TShock Contributors
+// 
+// This file is part of TShock.
+// 
+// TShock is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// TShock is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with TShock.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+
+namespace TShock.Commands.Parsers {
+    /// <summary>
+    /// An attribute that can be applied to a <c>bool</c> parameter to specify flag parsing for that specific parameter.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class FlagAttribute : Attribute {
+        /// <summary>
+        /// Gets the short form of the flag.
+        /// </summary>
+        public char ShortFlag { get; }
+
+        /// <summary>
+        /// Gets the long form of the flag.
+        /// </summary>
+        public string LongFlag { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FlagAttribute"/> class with the specified short and long flags.
+        /// </summary>
+        /// <param name="shortFlag">The short flag.</param>
+        /// <param name="longFlag">The long flag.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="longFlag"/> is <c>null</c>.</exception>
+        public FlagAttribute(char shortFlag, string longFlag) {
+            ShortFlag = shortFlag;
+            LongFlag = longFlag ?? throw new ArgumentNullException(nameof(longFlag));
+        }
+    }
+}
---
+++ tests/TShock.Tests/Commands/Parsers/FlagAttributeTests.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2019 Pryaxis & TShock Contributors
+// 
+// This file is part of TShock.
+// 
+// TShock is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// TShock is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with TShock.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace TShock.Commands.Parsers {
+    public class FlagAttributeTests {
+        [Fact]
+        public void Ctor_NullLongFlag_ThrowsArgumentNullException() {
+            Func<FlagAttribute> func = () => new FlagAttribute('f', null);
+
+            func.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void ShortFlag_Get_IsCorrect() {
+            var attribute = new FlagAttribute('f', "force");
+
+            attribute.ShortFlag.Should().Be('f');
+        }
+
+        [Fact]
+        public void LongFlag_Get_IsCorrect() {
+            var attribute = new FlagAttribute('f', "force");
+
+            attribute.LongFlag.Should().Be("force");
+        }
+    }
+}
