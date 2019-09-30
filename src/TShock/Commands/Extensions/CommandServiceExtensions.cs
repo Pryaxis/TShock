8fe4653eb3ecd4ad01f009f88e02e82ef2c8025b
Kevin Zhao <kzhao1729@gmail.com>
2019-09-29 21:16:09 -0700

    Add strongly-typed RegisterParser extension method.

---
+++ src/TShock/Commands/Extensions/CommandServiceExtensions.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) 2019 Pryaxis & TShock Contributors
+// 
+// This file is part of TShock.
+// 
+// TShock is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// TShock is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with TShock.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using JetBrains.Annotations;
+using TShock.Commands.Parsers;
+
+namespace TShock.Commands.Extensions {
+    /// <summary>
+    /// Provides extensions to the <see cref="ICommandService"/> interface.
+    /// </summary>
+    public static class CommandServiceExtensions {
+        /// <summary>
+        /// Registers the given strongly-typed parser as the definitive parser for the parse type.
+        /// </summary>
+        /// <typeparam name="TParse">The parse type.</typeparam>
+        /// <param name="commandService">The command service.</param>
+        /// <param name="parser">The parser.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="commandService"/> or <paramref name="parser"/> are <c>null</c>.
+        /// </exception>
+        public static void RegisterParser<TParse>([NotNull] this ICommandService commandService,
+                                                  [NotNull] IArgumentParser<TParse> parser) {
+            if (commandService is null) throw new ArgumentNullException(nameof(commandService));
+            if (parser is null) throw new ArgumentNullException(nameof(parser));
+
+            commandService.RegisterParser(typeof(TParse), parser);
+        }
+    }
+}
---
+++ tests/TShock.Tests/Commands/Extensions/CommandServiceExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2019 Pryaxis & TShock Contributors
+// 
+// This file is part of TShock.
+// 
+// TShock is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// TShock is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with TShock.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using FluentAssertions;
+using Moq;
+using TShock.Commands.Parsers;
+using Xunit;
+
+namespace TShock.Commands.Extensions {
+    public class CommandServiceExtensionsTests {
+        [Fact]
+        public void RegisterParser1_IsCorrect() {
+            var parser = new Mock<IArgumentParser<byte>>().Object;
+            var mockCommandService = new Mock<ICommandService>();
+            mockCommandService.Setup(cs => cs.RegisterParser(typeof(byte), parser));
+
+            mockCommandService.Object.RegisterParser(parser);
+
+            mockCommandService.Verify(cs => cs.RegisterParser(typeof(byte), parser));
+            mockCommandService.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void RegisterParser1_NullCommandService_ThrowsArgumentNullException() {
+            var parser = new Mock<IArgumentParser<byte>>().Object;
+            Action action = () => CommandServiceExtensions.RegisterParser(null, parser);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void RegisterParser1_NullParser_ThrowsArgumentNullException() {
+            var commandService = new Mock<ICommandService>().Object;
+            Action action = () => commandService.RegisterParser<byte>(null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+    }
+}
