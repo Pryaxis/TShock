9dff07e6c2cc113dd07c0c7d76639d4147fb75ab
Chris <2648373+QuiCM@users.noreply.github.com>
2020-06-02 19:18:43 +0930

    Refactor EmojiHandler to implement IPacketHandler<TArgs>

--- TShockAPI/Handlers/EmojiHandler.cs
+++ TShockAPI/Handlers/EmojiHandler.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static TShockAPI.GetDataHandlers;
 
 namespace TShockAPI.Handlers
 {
 	/// <summary>
 	/// Handles emoji packets and checks for validity and permissions
 	/// </summary>
-	public class EmojiHandler
+	public class EmojiHandler : IPacketHandler<EmojiEventArgs>
 	{
-		public void OnReceiveEmoji(object sender, GetDataHandlers.EmojiEventArgs args)
+		/// <summary>
+		/// Invoked when an emoji is sent in chat. Rejects the emoji packet if the player is spoofing IDs or does not have emoji permissions
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		public void OnReceive(object sender, EmojiEventArgs args)
 		{
 			if (args.PlayerIndex != args.Player.Index)
 			{
