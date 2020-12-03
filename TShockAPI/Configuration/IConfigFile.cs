a03647ce3836225e486f2d24a945beb8a2223a16
Chris <2648373+QuiCM@users.noreply.github.com>
2020-12-03 17:42:46 +1030

    Genericise configurations
    This is a breaking change. Enables easy creation of config files without copy/pasting the Read/Write code all the time

---
+++ TShockAPI/Configuration/IConfigFile.cs
@@ -0,0 +1,55 @@
+﻿namespace TShockAPI.Configuration
+{
+	/// <summary>
+	/// Describes a generic configuration interface wrapping some settings
+	/// </summary>
+	/// <typeparam name="TSettings"></typeparam>
+	public interface IConfigFile<TSettings>
+	{
+		/// <summary>
+		/// Settings managed by this config file
+		/// </summary>
+		TSettings Settings { get; set; }
+
+		/// <summary>
+		/// Reads settings from a given path
+		/// </summary>
+		/// <param name="path">The path to the file containing the settings</param>
+		/// <param name="incompleteSettings">
+		/// Whether the settings object has any new fields in it, meaning that the configuration should be
+		/// overwritten.
+		/// </param>
+		/// <returns>Settings object</returns>
+		TSettings Read(string path, out bool incompleteSettings);
+		/// <summary>
+		/// Reads settings from a given stream
+		/// </summary>
+		/// <param name="stream">The stream containing the settings</param>
+		/// <param name="incompleteSettings">
+		/// Whether the settings object has any new fields in it, meaning that the configuration should be
+		/// overwritten.
+		/// </param>
+		/// <returns>Settings object</returns>
+		TSettings Read(System.IO.Stream stream, out bool incompleteSettings);
+
+		/// <summary>
+		/// Converts a json-formatted string into the settings object used by this configuration
+		/// </summary>
+		/// <param name="json">Json string to parse</param>
+		/// <param name="incompleteSettings">Whether or not the json string contained an incomplete set of settings</param>
+		/// <returns>Settings object</returns>
+		TSettings ConvertJson(string json, out bool incompleteSettings);
+
+		/// <summary>
+		/// Writes this configuration to a given path
+		/// </summary>
+		/// <param name="path">File location the configuration will be written to</param>
+		void Write(string path);
+		/// <summary>
+		/// Writes this configuration to a stream
+		/// </summary>
+		/// <param name="stream">Stream the configuration will be written to</param>
+		void Write(System.IO.Stream stream);
+
+	}
+}
---
+++ TShockAPI/Configuration/ServerSideConfig.cs
@@ -0,0 +1,113 @@
+/*
+TShock, a server mod for Terraria
+Copyright (C) 2011-2019 Pryaxis & TShock Contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TShockAPI.Configuration
+{
+	/// <summary>
+	/// Settings used to configure server side characters
+	/// </summary>
+	public class SscSettings
+	{
+		/// <summary>
+		/// Enable server side characters, causing client data to be saved on the server instead of the client.
+		/// </summary>
+		[Description("Enable server side characters, causing client data to be saved on the server instead of the client.")]
+		public bool Enabled = false;
+
+		/// <summary>
+		/// How often SSC should save, in minutes.
+		/// </summary>
+		[Description("How often SSC should save, in minutes.")]
+		public int ServerSideCharacterSave = 5;
+
+		/// <summary>
+		/// Time, in milliseconds, to disallow discarding items after logging in when ServerSideCharacters is ON.
+		/// </summary>
+		[Description("Time, in milliseconds, to disallow discarding items after logging in when ServerSideCharacters is ON.")]
+		public int LogonDiscardThreshold = 250;
+
+		/// <summary>
+		/// The starting default health for new players when SSC is enabled.
+		/// </summary>
+		[Description("The starting default health for new players when SSC is enabled.")]
+		public int StartingHealth = 100;
+
+		/// <summary>
+		/// The starting default mana for new players when SSC is enabled.
+		/// </summary>
+		[Description("The starting default mana for new players when SSC is enabled.")]
+		public int StartingMana = 20;
+
+		/// <summary>
+		/// The starting default inventory for new players when SSC is enabled.
+		/// </summary>
+		[Description("The starting default inventory for new players when SSC is enabled.")]
+		public List<NetItem> StartingInventory = new List<NetItem>();
+	}
+
+	/// <summary>
+	/// Configuration for the server side characters system
+	/// </summary>
+	public class ServerSideConfig : ConfigFile<SscSettings>
+	{
+		/// <summary>
+		/// Settings managed by this configuration object
+		/// </summary>
+		public override SscSettings Settings { get; set; } = new SscSettings();
+
+		/// <summary>
+		/// Dumps all configuration options to a text file in Markdown format
+		/// </summary>
+		public static void DumpDescriptions()
+		{
+			var sb = new StringBuilder();
+			var defaults = new ServerSideConfig();
+
+			foreach (var field in defaults.GetType().GetFields().OrderBy(f => f.Name))
+			{
+				if (field.IsStatic)
+					continue;
+
+				var name = field.Name;
+				var type = field.FieldType.Name;
+
+				var descattr =
+					field.GetCustomAttributes(false).FirstOrDefault(o => o is DescriptionAttribute) as DescriptionAttribute;
+				var desc = descattr != null && !string.IsNullOrWhiteSpace(descattr.Description) ? descattr.Description : "None";
+
+				var def = field.GetValue(defaults);
+
+				sb.AppendLine("{0}  ".SFormat(name));
+				sb.AppendLine("Type: {0}  ".SFormat(type));
+				sb.AppendLine("Description: {0}  ".SFormat(desc));
+				sb.AppendLine("Default: \"{0}\"  ".SFormat(def));
+				sb.AppendLine();
+			}
+
+			File.WriteAllText("ServerSideConfigDescriptions.txt", sb.ToString());
+		}
+	}
+}
