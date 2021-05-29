258b21dddb9d89526a50dc5fba2343679b8b64a7
Lucas Nicodemus <shank@shanked.me>
2021-05-28 20:21:14 -0700

    Revert "Merge branch 'patch-28' into general-devel"
    This reverts commit d2179e95ffd1cc7647febd69d10e7483667efeb9, reversing
    changes made to 58d7e26960138186e2182df3861dcea7bc3df739.

--- TShockAPI/Bouncer.cs
+++ TShockAPI/Bouncer.cs
@@ -308,11 +308,10 @@ namespace TShockAPI
 						return;
 					}
 
-					if ((args.Player.TPlayer.BiomeTorchHoldStyle(style) != args.Player.TPlayer.BiomeTorchPlaceStyle(style))
-					&& (selectedItem.placeStyle != style))
+					if (selectedItem.placeStyle != style)
 					{
-						TShock.Log.ConsoleError("Bouncer / OnTileEdit rejected from (placestyle) {0} {1} {2} placeStyle: {3} expectedStyle: {4}",
-							args.Player.Name, action, editData, style, selectedItem.placeStyle);
+						TShock.Log.ConsoleError(string.Format("Bouncer / OnTileEdit rejected from (placestyle) {0} {1} {2} placeStyle: {3} expectedStyle: {4}",
+							args.Player.Name, action, editData, style, selectedItem.placeStyle));
 						args.Player.SendTileSquare(tileX, tileY, 1);
 						args.Handled = true;
 						return;
