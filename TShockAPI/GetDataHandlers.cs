a0cd2849eddb2f07052b8163679860ca31f8a912
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-18 17:15:36 +0800

    Add missing entries to ExtraneousPlaceStyles

--- TShockAPI/GetDataHandlers.cs
+++ TShockAPI/GetDataHandlers.cs
@@ -4256,6 +4256,12 @@ namespace TShockAPI
 		/// </summary>
 		internal static Dictionary<int, int> ExtraneousPlaceStyles = new Dictionary<int, int>
 		{
+			{TileID.Jackolanterns, 8},
+			{TileID.Presents, 6},
+			{TileID.Explosives, 1},
+			{TileID.Crystals, 17},
+			{TileID.SnowballLauncher, 1},
+			{TileID.Painting4X3, 8},
 			{TileID.MinecartTrack, 3}
 		};
 		
