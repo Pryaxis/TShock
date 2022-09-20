9af7f6715adb311b804567bec7163424d2d8f19a
Chris <2648373+QuiCM@users.noreply.github.com>
2022-09-20 20:41:33 +0930

    Add configurators for TShock configuration & logging

---
+++ TShockAPI/Configurators/ServerConfigConfigurator.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using TerrariaApi.Configurator;
+
+
+namespace TShockAPI.Configurators;
+
+/// <summary>
+/// Configures the configuration provider, adding TShock's configuration options
+/// </summary>
+public class ServerConfigConfigurator : ConfigConfigurator
+{
+	const string DEFAULT_PATH = "tshock.json";
+
+	/// <summary>
+	/// Construct a new ServerConfigConfigurator, setting a priority of 100
+	/// </summary>
+	public ServerConfigConfigurator() : base()
+	{
+		Priority = 100;
+	}
+
+	/// <inheritdoc/>
+	public override void Configure(HostBuilderContext hostContext, IConfigurationBuilder configBuilder, string[] args)
+	{
+		// Configuration is loaded as such:
+		// commandline > environment vars > json configuration.
+		// Environment variables and commandline are built first here, to determine if an alternative config path has been set.
+		IConfiguration tshockEnvVars = new ConfigurationBuilder()
+			.AddEnvironmentVariables("TSHOCK_")
+			.AddCommandLine(args)
+			.Build();
+
+		string configPath = tshockEnvVars.GetSection("configpath").Value ?? DEFAULT_PATH;
+
+		// We now build a second IConfiguration that first takes values from the json file, 
+		// then overwrites any duplicates with values from the environment variables, then the commandline
+		IConfiguration tshockConfig = new ConfigurationBuilder()
+			.AddJsonFile(configPath)
+			.AddConfiguration(tshockEnvVars)
+			.Build();
+
+		configBuilder.AddConfiguration(tshockConfig);
+	}
+}
