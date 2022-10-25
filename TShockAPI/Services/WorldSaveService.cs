683d51bb0233d32315d8bb8abd442ab9b44ed865
Chris <2648373+QuiCM@users.noreply.github.com>
2022-10-25 22:52:38 +1030

    Quick implementation of BackupService

---
+++ TShockAPI/Services/WorldSaveService.cs
@@ -0,0 +1,18 @@
+
+using TerrariaApi.Server;
+
+namespace TShockAPI.Services;
+
+/// <summary>
+/// Service providing world saving functionality.
+/// </summary>
+public sealed class WorldSaveService : PluginService
+{
+	/// <summary>
+	/// Saves the world.
+	/// </summary>
+	public void SaveWorld()
+	{
+
+	}
+}
