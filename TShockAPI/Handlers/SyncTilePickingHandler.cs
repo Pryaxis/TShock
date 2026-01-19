3df65c7e2b293563951eb6f9fee4c40762a4e14d
lost-werewolf <sebmartinpersonal@gmail.com>
2026-01-19 16:03:24 -0500

    also reject for disabled/throttled players

--- TShockAPI/Handlers/SyncTilePickingHandler.cs
+++ TShockAPI/Handlers/SyncTilePickingHandler.cs
@@ -25,7 +25,8 @@ namespace TShockAPI.Handlers
 				return;
 			}
 
-			if (!args.Player.HasBuildPermission(args.TileX, args.TileY) || !args.Player.IsInRange(args.TileX, args.TileY))
+			if (args.Player.IsBeingDisabled() || args.Player.IsBouncerThrottled() ||
+				!args.Player.HasBuildPermission(args.TileX, args.TileY) || !args.Player.IsInRange(args.TileX, args.TileY))
 			{
 				// Reject silently without logging. This packet is sent when TileEdit is sent, no need to double-log.
 				// It's not possible to send correcting data for this packet.
