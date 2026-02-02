8ddd172e3cad7ba4d7064dfa4c53b824f300bc41
Cai <13110818005@qq.com>
2026-02-02 22:19:24 +0800

    Add `/bossdamage` command from 1.4.5

--- TShockAPI/Commands.cs
+++ TShockAPI/Commands.cs
@@ -36,6 +36,7 @@ using Microsoft.Xna.Framework;
 using TShockAPI.Localization;
 using System.Text.RegularExpressions;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.GameContent.Creative;
 
 namespace TShockAPI
@@ -646,6 +647,10 @@ namespace TShockAPI
 			add(new Command(ShowAllPVPDeath, "allpvpdeath")
 			{
 				HelpText = GetString("Shows the number of PVP deaths for all online players."),
+			});
+			add(new Command(BossDamage, "bossdamage")
+			{
+				HelpText = GetString("Shows recent boss kill contribution."),
 			});
 
 			TShockCommands = new ReadOnlyCollection<Command>(tshockCommands);
@@ -5912,6 +5917,27 @@ namespace TShockAPI
 				.Select(x => GetString($"*{x.Name} was slain by other players {x.DeathsPVP} times."));
 
 			args.Player.SendErrorMessage(string.Join('\n',deathsRank));
+		}
+
+		private static void BossDamage(CommandArgs args)
+		{
+			var attempts = NPCDamageTracker.RecentAttempts().ToList();
+
+			if (attempts.Count == 0)
+			{
+				args.Player.SendWarningMessage(GetString("No recent boss kill data found."));
+				return;
+			}
+			foreach (var recentAttempt in attempts)
+			{
+				for (var playerId = 0; playerId < byte.MaxValue; ++playerId)
+				{
+					if (Main.player[playerId].active)
+					{
+						args.Player.SendSuccessMessage(recentAttempt.GetReport(Main.player[playerId]).ToString());
+					}
+				}
+			}
 		}
 
 
