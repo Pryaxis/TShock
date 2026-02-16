94da388070ab799a4f18ab91e768aa897acc07d3
Pace Kobo <abelhuwaa07@gmail.com>
2026-02-17 03:15:20 +0900

    Update TShock.cs

--- TShockAPI/TShock.cs
+++ TShockAPI/TShock.cs
@@ -1770,7 +1770,7 @@ namespace TShockAPI
 		{
 			if (e.MsgId == PacketTypes.PlayerHp)
 			{
-				if (Main.player[(byte)e.number].statLife <= 0)
+				if (Main.player[(byte)e.number].statLife <= 0 && !Players[e.number].Dead)
 				{
 					e.Handled = true;
 					return;
