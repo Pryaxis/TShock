451a056187904b5773173deca87725e93fafae8a
ksqeib <ksqeib@qq.com>
2026-02-21 18:07:07 +0800

    refactor: extract ResolveSpawnpoint to remove HandleSpawn/TeleportSpawnpoint duplication

--- TShockAPI/TSPlayer.cs
+++ TShockAPI/TSPlayer.cs
@@ -1441,14 +1441,7 @@ namespace TShockAPI
 		public bool TeleportSpawnpoint()
 		{
 			// NOTE: it is vanilla behaviour to not permanently override the spawnpoint if the bed spawn is broken/invalid
-			int x = TPlayer.SpawnX;
-			int y = TPlayer.SpawnY;
-			if ((x == -1 && y == -1) || 
-				!Main.tile[x, y - 1].active() || Main.tile[x, y - 1].type != TileID.Beds || !WorldGen.StartRoomCheck(x, y - 1))
-			{
-				x = Main.spawnTileX;
-				y = Main.spawnTileY;
-			}
+			GetDataHandlers.ResolveSpawnpoint(TPlayer, out int x, out int y);
 			return Teleport(x * 16, y * 16 - 48);
 		}
 
