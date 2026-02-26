00c72efffda97fdc997abc6bbe19741360eaeda5
Cai <13110818005@qq.com>
2026-02-26 21:18:53 +0800

    Add a option `AllowCtTag`

--- TShockAPI/TShock.cs
+++ TShockAPI/TShock.cs
@@ -1494,8 +1494,12 @@ namespace TShockAPI
 			// We should now use the truncated message instead of the original, we don't want anything to fire off on text that has been "removed"...
 			// Yes, double assignment like this looks bad...
 
-			text = Regex.Replace(text, @"\[ct:[^\]]*\]", "");
+
 			// Filter out [ct:xxx] tags because they may crash the PE client
+			if (!Config.Settings.AllowCtTag)
+			{
+				text = Regex.Replace(text, @"\[ct:[^\]]*\]", "");
+			}
 
 			var chatText = text;
 
