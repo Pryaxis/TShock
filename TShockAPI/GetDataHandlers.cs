9a6c6a36d387cca86fada58e2d60baa4397af0ec
Xekep <Xekep@users.noreply.github.com>
2026-02-28 09:20:10 +0300

    Fix first SSC login appearance reset for new players

--- TShockAPI/GetDataHandlers.cs
+++ TShockAPI/GetDataHandlers.cs
@@ -2807,6 +2807,11 @@ namespace TShockAPI
 						return true;
 
 					args.Player.PlayerData = TShock.CharacterDB.GetPlayerData(args.Player, account.ID);
+					if (Main.ServerSideCharacter && TShock.CharacterDB.IsSeededAppearanceMissing(args.Player.PlayerData))
+					{
+						TShock.CharacterDB.SyncSeededAppearance(account, args.Player);
+						args.Player.PlayerData = TShock.CharacterDB.GetPlayerData(args.Player, account.ID);
+					}
 
 					args.Player.Group = group;
 					args.Player.tempGroup = null;
@@ -3432,6 +3437,11 @@ namespace TShockAPI
 				{
 					args.Player.RequiresPassword = false;
 					args.Player.PlayerData = TShock.CharacterDB.GetPlayerData(args.Player, account.ID);
+					if (Main.ServerSideCharacter && TShock.CharacterDB.IsSeededAppearanceMissing(args.Player.PlayerData))
+					{
+						TShock.CharacterDB.SyncSeededAppearance(account, args.Player);
+						args.Player.PlayerData = TShock.CharacterDB.GetPlayerData(args.Player, account.ID);
+					}
 
 					if (args.Player.State == (int)ConnectionState.AssigningPlayerSlot)
 						args.Player.State = (int)ConnectionState.AwaitingPlayerInfo;
