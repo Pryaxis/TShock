e0e34dcfe4f09f5e4f800276cebeb06ff09f44c7
Cai <13110818005@qq.com>
2026-03-17 12:41:51 +0800

    Move resizechests permission to ignore node

--- TShockAPI/Permissions.cs
+++ TShockAPI/Permissions.cs
@@ -172,6 +172,10 @@ namespace TShockAPI
 
 		[Description("Prevents you from being disabled by abnormal MP.")]
 		public static readonly string ignoremp = "tshock.ignore.mp";
+
+
+		[Description("Player can resize chests. Warning: Dangerous permission to grant, very easy to abuse.")]
+		public static readonly string resizechests = "tshock.ignore.resizechests";
 		#endregion
 
 		#region tshock.item nodes
@@ -515,9 +519,6 @@ namespace TShockAPI
 
 		[Description("Player can send emotes.")]
 		public static readonly string sendemoji = "tshock.sendemoji";
-
-		[Description("Player can resize chests. Warning: Dangerous permission to grant, very easy to abuse.")]
-		public static readonly string resizechests = "tshock.resizechests";
 		#endregion
 		/// <summary>
 		/// Lists all commands associated with a given permission
